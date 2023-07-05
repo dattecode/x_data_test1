049df860d6ba2f5ed1542e6817b58920efdd2b86
dattecode <dattecode@gmail.com>
2023-07-04 23:39:49 -0400

    1 commit

---
+++ src/main.js
@@ -0,0 +1,103 @@
+
+let pj_final_fantasy = [
+    {ff_1 : [
+        "Arylon", "Bahamult", "chaos"
+    ]},
+    {ff_2 : [
+        "Firion", "Maria", "Guy", "Josef"
+    ]},
+    {ff_3 : [
+        "Luneth", "Arc", "Refia", "Cid", "Refia"
+    ]},
+    {ff_4: [
+        "Cecil Harvey", "Rosa Farrel", "Cain", "Golbez", "Edward"
+    ]},
+    {ff_5 : [
+        "Bartz Klauder", "Gilgamesh", "Galuf", "Cid Previa"
+    ]}
+]
+
+let pj_final_fantasyS2 = [
+    {ff_6 : [
+        "Terra Branford", "Locke Cole", "Celes Chere", "Edgar Figaro", "Sabin Figaro"
+    ]},
+    {ff_7 : [
+        "Cloud Strife", "Tifa Lockhart", "Aerith Gainsborough", "Barret Wallace", "Sephiroth"
+    ]},
+    {ff_8 : [
+        "Squall Leonhart", "Rinoa Heartilly", "Quistis Trepe", "Zell Dincht", "Selphie Tilmitt"
+    ]},
+    {ff_9 : [
+        "Zidane Tribal", "Garnet Til Alexandros XVII", "Vivi Ornitier", "Steiner", "Freya Crescent"
+    ]}
+]
+
+let superheroes = [
+    {dc_comics : [
+        "Batman", "Superman", "Wonder Woman", "Flash"
+    ]},
+    {marvel : [
+        "Iron Man", "Spider-Man", "Thor", "Black Widow"
+    ]},
+    {xmen : [
+        "Wolverine", "Cyclops", "Storm", "Jean Grey"
+    ]},
+    {avengers : [
+        "Captain America", "Hulk", "Black Panther", "Captain Marvel"
+    ]}
+]
+
+let paises = [
+    {america : [
+        "Estados Unidos", "Canadá", "Brasil", "México"
+    ]},
+    {europa : [
+        "España", "Francia", "Alemania", "Italia"
+    ]},
+    {asia : [
+        "China", "Japón", "India", "Corea del Sur"
+    ]},
+    {africa : [
+        "Egipto", "Sudáfrica", "Kenia", "Marruecos"
+    ]}
+]
+
+let deportes = [
+    {futbol : [
+        "Lionel Messi", "Cristiano Ronaldo", "Neymar Jr.", "Mohamed Salah"
+    ]},
+    {basketball : [
+        "LeBron James", "Stephen Curry", "Kevin Durant", "Giannis Antetokounmpo"
+    ]},
+    {tenis : [
+        "Roger Federer", "Rafael Nadal", "Novak Djokovic", "Serena Williams"
+    ]}
+]
+
+
+const groupsHtml = document.querySelector(".groups")
+
+function tranform_data (arr, nameArr) {
+
+    let html = `<div class"group">${nameArr}</div>` // inner 1
+    groupsHtml.innerHTML = html
+    for (const arrIter in arr) {
+        let obj = arrIter
+
+        for (key of obj) {
+            let objArr = Object.values(obj)[0]
+            let gp_name = `<div class="subGroup">${Object.keys(obj)}
+                                <div class="partecipants">
+                                </div>
+                            </div>` // inner 2 a 1
+            document.querySelector(".group").innerHTML = gp_name
+            for (const u in objArr) {
+                element += `<div class="part">
+                                <p>${u}</p>
+                            </div>` // iner 2 a 3
+            }
+            let element = ""
+            document.querySelector(".subGroup:last-child .partecipants").innerHTML = element
+        }
+    }
+}
